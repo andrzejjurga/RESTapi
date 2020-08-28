26d82420cd80b98e7c67213b25fd7bc8fbce9fad
Andrzej Jurga <59994218+andrzejjurga@users.noreply.github.com>
2020-08-28 17:18:30 +0200

    Add files via upload

--- rest_api/src/main/java/com/example/restservice/Greeting.java
+++ rest_api/src/main/java/com/example/restservice/Greeting.java
@@ -1,13 +1,15 @@
 package com.example.restservice;
-
+import java.util.Date;
 public class Greeting {
 
 	private final long id;
 	private final String content;
+	private final Date time;
 
-	public Greeting(long id, String content) {
+	public Greeting(long id, String content, Date time) {
 		this.id = id;
 		this.content = content;
+		this.time = time;
 	}
 
 	public long getId() {
@@ -16,5 +18,9 @@ public class Greeting {
 
 	public String getContent() {
 		return content;
+	}
+
+	public Date getTime() {
+		return time;
 	}
 }
